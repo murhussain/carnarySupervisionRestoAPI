3231893f7332eef1f87425efa8967667e36787d4
murhussain <husseinmurashi@yahoo.com>
2021-12-31 08:42:16 +0200

    m

--- restaurantAPI/settings.py
+++ restaurantAPI/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
 ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # MEDIA_URL = "http://127.0.0.1:8000/media/"
 
