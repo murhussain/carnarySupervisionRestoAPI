2517a04825ffbcf3b4d652174b7e116b4d1b877e
murhussain <husseinmurashi@yahoo.com>
2021-12-31 08:39:26 +0200

    s

--- restaurantAPI/settings.py
+++ restaurantAPI/settings.py
@@ -135,8 +135,8 @@ STATICFILES_DIRS = [
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
-MEDIA_URL = "http://127.0.0.1:8000/media/"
+MEDIA_URL = '/media/'
+# MEDIA_URL = "http://127.0.0.1:8000/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
